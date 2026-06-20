3f649b54cda9b1cd36b713cbb450985d62e17ecf
gauravanand10 <ganand_be23@thapar.edu>
2026-06-21 00:57:09 +0530

    Create half_adder.v

---
+++ Day_3/rtl/half_adder.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+
+module half_adder
+(
+    input a_in, b_in,
+    output sum_out, carry_out
+    );
+    
+assign sum_out = a_in ^ b_in;
+assign carry_out = a_in & b_in;
+
+endmodule
