352eb36f19917490b15b10811c760f9a2b108f5d
gauravanand10 <ganand_be23@thapar.edu>
2026-06-23 13:58:21 +0530

    Add testbench for input_to_output module

---
+++ Day_5/tb/input_to_output_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+
+module input_to_output_tb;
+        reg a_in;
+        wire b_out;
+
+input_to_output uut(
+        .a_in(a_in),
+        .b_out(b_out)
+);
+
+        initial begin
+	
+ 	      // Generate waveform file for GTKWave
+        $dumpfile("wave.vcd");
+        $dumpvars(0, input_to_output_tb);  
+
+        // Monitor signals in terminal
+        $monitor("Time = %0t | a_in = %b | b_out = %b",
+                 $time, a_in, b_out);
+     
+        // Test cases
+        a_in = 0;#10;
+        a_in = 1;#10;
+        a_in = 0;#10;
+        a_in = 1;#10;
+
+        #10 $finish;
+end
+endmodule
