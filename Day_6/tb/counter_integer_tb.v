e1f3ea00f5542b80aa88b78905c428703f128927
gauravanand10 <ganand_be23@thapar.edu>
2026-06-24 15:30:25 +0530

    Create counter_integer_tb.v

---
+++ Day_6/tb/counter_integer_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module counter_integer_tb;
+
+	reg clk;
+	reg rst;
+
+	counter uut (
+		.clk(clk),
+		.rst(rst)
+);
+
+	always #5 clk = ~clk;
+	
+	initial begin 
+		clk = 0; rst = 1;
+		
+		#20;
+		rst = 0;
+		
+		#3000;
+		
+		$finish;
+	end
+	
+        // Monitor Counter Value
+        initial begin
+        $monitor("Time=%0t rst=%b clk=%b counter=%0d",
+                  $time, rst, clk, uut.counter);
+                  
+	// Generate waveform file for GTKWave
+    	$dumpfile("wave.vcd");
+    	$dumpvars(0, counter_tb);
+
+end
+endmodule
