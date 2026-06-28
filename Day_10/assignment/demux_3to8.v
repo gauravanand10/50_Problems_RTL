022147d87b402dd4275c783d2e17ce115a46ceb7
gauravanand10 <ganand_be23@thapar.edu>
2026-06-28 22:26:09 +0530

    Create demux_3to8.v

---
+++ Day_10/assignment/demux_3to8.v
@@ -0,0 +1,27 @@
+module demux_3to8(
+	input data_in,
+	input [2:0] sel,
+
+	output reg [7:0] data_out 
+);
+
+	always@ (*) begin
+		
+		data_out = 8'b00000000;
+		
+	case(sel)
+	
+		3'b000: data_out[0] = data_in;
+        	3'b001: data_out[1] = data_in;
+        	3'b010: data_out[2] = data_in;
+        	3'b011: data_out[3] = data_in;
+        	3'b100: data_out[4] = data_in;
+        	3'b101: data_out[5] = data_in;
+        	3'b110: data_out[6] = data_in;
+        	3'b111: data_out[7] = data_in;
+        	
+		default : data_out = 8'b00000000;
+	endcase
+
+end 
+endmodule
